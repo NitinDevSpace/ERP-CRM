7e2a38d82dbb4ee26f121f3906970a882cb837cf
Nitin <nitindevspace@gmail.com>
2025-11-20 17:40:20 +0530

    Update package name to 'erp-crm', version to '5.1.0', and adjust API URLs for production environment

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,10 +1,10 @@
-export const API_BASE_URL =`http://localhost:10000/api/`;
-export const BASE_URL = `http://localhost:10000/`; 
+export const API_BASE_URL = `https://erp-crm-we20.onrender.com/api/`;
+export const BASE_URL = `https://erp-crm-we20.onrender.com/`; 
 export const WEBSITE_URL = 'https://erp-crm-we20.onrender.com/';
 export const DOWNLOAD_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
-    : 'http://localhost:10000/download/';
+    : 'https://erp-crm-we20.onrender.com/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
 
 export const FILE_BASE_URL = import.meta.env.VITE_FILE_BASE_URL;
